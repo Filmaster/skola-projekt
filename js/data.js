fc004ecf6f332c263cacf5bbc87ab42a5b71d8d8
Filmaster <36028787+Filmaster@users.noreply.github.com>
2019-03-21 19:35:49 +0100

    Asi hotovo

--- js/data.js
+++ js/data.js
@@ -1,96 +1,87 @@
-var prezidenti = [
+var hodnosti = [
     {
-        "name":"Tomáš Garrigue Masaryk",
-        "inoffice":"1918-1935",
-        "description":"Český vědec, filozof, pedagog, politik a žurnalista zakladatel novodobého československého státu, československý prezident",
-        "photo":"prezident9.jpg",
-        "borned":"7. 3. 1850 Hodonín",
-        "died":"14. 9. 1937 Lány",    
-        "link":"https://www.hrad.cz/cs/prezident-cr/prezidenti-v-minulosti/tomas-garrigue-masaryk" 
+        "name":"Vojín",
+    "photo":"hodnost1.jpg"
     },
     {
-        "name":"Edvard Beneš",
-        "inoffice":"1935-1938, 1945-48",
-        "description":"Český a československý politik, diplomat, prezident 1935-48",
-        "photo":"prezident8.jpg",
-        "borned":"28. 5. 1884 Kožlany na Rakovnicku",
-        "died":"3. 9. 1948 Sezimovo Ústí",
-        "link":"https://www.hrad.cz/cs/prezident-cr/prezidenti-v-minulosti/edvard-benes" 
+        "name":"Svobodník",
+        "photo":"hodnost2.jpg"
     },
     {
-        "name":"Emil Hácha",
-        "inoffice":"1935-1938, 1945-48",
-        "description":"Český právník a státník, prezident Česko-Slovenské republiky a státní prezident Protektorátu Čechy a Morava",
-        "photo":"prezident5.jpg",
-        "borned":"12. 7. 1872 Trhové Sviny",
-        "died":"27. 6. 1945 Praha",
-        "link":"https://www.hrad.cz/cs/prezident-cr/prezidenti-v-minulosti/emil-hacha" 
+        "name":"Desátník",
+        "photo":"hodnost3.jpg"
     },
     {
-        "name":"Klement Gottwald",
-        "inoffice":"1948-53",
-        "description":"Komunistický politik a československý prezident",
-        "photo":"prezident7.jpg",
-        "borned":"23. 11. 1896 Dědice",
-        "died":"14. 3. 1953 Praha",
-        "link":"https://www.hrad.cz/cs/prezident-cr/prezidenti-v-minulosti/klement-gottwald" 
+        "name":"Četař",
+        "photo":"hodnost4.jpg"
     },
     {
-        "name":"Antonín Zápotocký",
-        "inoffice":"1953-57",
-        "description":"Český sociálně demokratický a poté československý komunistický politik a prezident",
-        "photo":"prezident10.jpg",
-        "borned":"19. 12. 1884 Zákolany",
-        "died":"13. 11. 1957 Praha",
-        "link":"https://www.hrad.cz/cs/prezident-cr/prezidenti-v-minulosti/antonin-zapotocky" 
+        "name":"Rotný",
+         "photo":"hodnost5.jpg"
+      
     },            
     {
-        "name":"Antonín Novotný",
-        "inoffice":"1957-68",
-        "description":"Československý komunistický politik a prezident",
-        "photo":"prezident6.jpg",
-        "borned":"10. 12. 1904 Praha-Letňany",
-        "died":"28. 1. 1975 Praha",
-        "link":"https://www.hrad.cz/cs/prezident-cr/prezidenti-v-minulosti/antonin-novotny" 
+        "name":" Rotmistr",
+       "photo":"hodnost6.jpg"
     },            
     {
-        "name":"Ludvík Svoboda",
-        "inoffice":"1968-75",
-        "description":"Československý armádní generál, komunistický politik a  prezident v letech 1968 - 1975",
-        "photo":"prezident4.jpg",
-        "borned":"25. 11. 1895 Hroznatín",
-        "died":"20. 9. 1979 Praha",
-        "link":"https://www.hrad.cz/cs/prezident-cr/prezidenti-v-minulosti/ludvik-svoboda" 
-    },            
+        "name":"nadrotmistr",
+       "photo":"hodnost7.jpg"
+    },
     {
-        "name":"Gustáv Husák",
-        "inoffice":"1975-89",
-        "description":"Slovenský právník, československý komunistický politik a prezident",
-        "photo":"prezident3.jpg",
-        "borned":"10. 1. 1913 Bratislava-Dúbravka",
-        "died":"18. 11. 1991 Bratislava",
-        "link":"https://www.hrad.cz/cs/prezident-cr/prezidenti-v-minulosti/gustav-husak" 
+        "name":"praporčík ",
+       "photo":"hodnost8.jpg"
+    },
+    {
+        "name":"nadpraporčík ",
+        "photo":"hodnost9.jpg"
+    },
+    {
+        "name":"štábní praporčík ",
+        "photo":"hodnost10.jpg"
+    },
+    {
+        "name":"poručík ",
+        "photo":"hodnost11.jpg"
+    },
+    {
+        "name":"nadporučík",
+       "photo":"hodnost12.jpg"
+    },
+    {
+        "name":"kapitán ",
+       "photo":"hodnost13.jpg"
+    },
+    {
+        "name":"major ",
+       "photo":"hodnost14.jpg"
+    },
+
+     {
+        "name":"podplukovnník ",
+      "photo":"hodnost15.jpg"
+    },
+    {
+        "name":"plukovník ",
+        "photo":"hodnost16.jpg"
+    },
+    {
+        "name":"brigádní generál ",
+       "photo":"hodnost17.jpg"
+    },
+    {
+        "name":"generálmajor ",
+        "photo":"hodnost18.jpg"
+    },
+    {
+        "name":"generálporučík ",
+        "photo":"hodnost19.jpg"
+    },
+    {
+        "name":"armádní generál",
+        "photo":"hodnost20.jpg"
     }            
 ];
-
-var sidla = [
-    {
-        "place":"Pražský hrad",
-        "description": "Pražský hrad je naší nejvýznamnější národní kulturní a historickoupamátkou, je symbolem více než tisíciletého vývoje českého státu. Je to monumentální komplex palácových, církevních, fortifikačních, úředních a obytných staveb, které jsou vzácnými památkami na všechna slohová období. Rozkládá se na ploše 45 ha. Byl sídlem českých knížat, králů a císařů a od vzniku republiky v r. 1918 i sídlem prezidentů.",
-        "photo": "prazsky-hrad.jpg"
-    },
-    {
-        "place":"Zámek Lány u Prahy",
-        "description": "Zámek Lány je barokní stavba, jež se nachází na jižním okraji obce Lány v okrese Kladno ve Středočeském kraji západně od Prahy. Zámek je situován na severním okraji rozsáhlých křivoklátských lesů (CHKO Křivoklátsko), jeho součástí je rozsáhlý anglický park, rybník Bahňák, velký palmový skleník a přilehlá Lánská obora. K zámku také patřil hospodářský dvůr. Od roku 1921 je zámek majetkem státu a stal se oficiálním letním sídlem prezidentů Československa, dnes prezidentů České republiky.",
-        "photo": "zamek-lany.jpg"
-    },
-    {
-        "place":"Zámek Topolčianky",
-        "description": "V letech 1923–39 a 1945–51 sloužil jako letní sídlo československých prezidentů, 1939–45 jako letní sídlo prezidenta Slovenské republiky. Nejvíce je spjat s pobyty prezidenta Masaryka ve 20. letech a na počátku 30. let. Za slovenské letní sídlo prezidenta ČSR byl vybrán se zřetelem na svůj dobrý stav, fungující železniční spojení s Prahou, příjemné klima a anglický park. Na jaře 1923 prošel rychlými úpravami tak, aby mohl prezidenta i s doprovodem poprvé uvítat už 29. 7. 1923. Pro zajištění rychlého spojení s Prahou byla zde v době zdejšího prezidentova pobytu zřízena letecká pošta, posilněny telefonní a telegrafní ústředny a na louce Lazy vybudováno polní letiště.",
-        "photo": "zamek-topolcianky.jpg"
-    }
-];
-
 
 var perlicky = [
     {
